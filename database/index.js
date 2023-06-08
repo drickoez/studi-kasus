ee8ae003130dc81f074519baaabaab3f24a07826
drickoez <dimasricko007@gmail.com>
2023-06-08 21:57:15 +0700

    initial commit

---
+++ database/index.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const { dbHost, dbName, dbPass, dbPort, dbUser } = require("../app/config");
+
+mongoose.connect(
+  `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=db_eduwork`
+);
+
+const db = mongoose.connection;
+
+module.exports = db;
